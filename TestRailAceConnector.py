dd2cfc51e14d4fa027fb21c298176c40f0795d5f
richard.thomas <richard.thomas@mercurygate.com>
2017-05-19 11:28:40 -0400

    Added port specification to Flask app for Heroku issues

--- TestRailAceConnector.py
+++ TestRailAceConnector.py
@@ -59,4 +59,6 @@ def main():
     #connector.getOpenTestRuns()
     return "This worked"
 
-if __name__ =='__main__':main()
+if __name__ =='__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
